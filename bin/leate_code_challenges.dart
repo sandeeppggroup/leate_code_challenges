37fd24bb4d65cbcdde9e52d3a459effbf5260751
sandeep abraham <sandeep.pggroup@gmail.com>
2023-12-02 14:37:44 +0530

    s

--- bin/leate_code_challenges.dart
+++
@@ -1,5 +0,0 @@
-import 'package:leate_code_challenges/leate_code_challenges.dart' as leate_code_challenges;
-
-void main(List<String> arguments) {
-  print('Hello world: ${leate_code_challenges.calculate()}!');
-}
