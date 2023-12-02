26f05437858551fc609a4a540421b8877335c818
sandeep abraham <sandeep.pggroup@gmail.com>
2023-12-02 14:35:26 +0530

    third commit

--- test/leate_code_challenges_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:leate_code_challenges/leate_code_challenges.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
